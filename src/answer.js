a9208c680777cac9df87aa71a647cb11fdc724ef
mingdazhang2 <449144331@qq.com>
2017-09-14 11:24:41 +1200

    jsDoc

--- src/answer.js
+++ src/answer.js
@@ -1,18 +1,28 @@
 /* jshint undef: true, unused: true, esversion: 6, asi: true, browser: true, jquery: true */
 
 class Answer {
+  /**
+   * The constructor function of Answer
+   * @param  {String} - Answer Text
+   * 
+   */
   constructor (newAnswerText) {
     this.answerText = newAnswerText
     this.answerScore = 0
     this.incorrectAnswerTime = 0
   }
-		/*Set the answer score
-		*/ 
+		/**
+     * Set the score  
+     * @param {Number} - Score number  
+     */
   setAnswerScore (newScore) {
     this.answerScore = newScore
   }
-		/*Calculate the score
-		*/ 
+		/**
+     * Calculate the score
+     * @param  {Number} - Score number
+     * @return {Number} - The score after calulating
+     */
   reduceAnswerScore (number) {
     this.answerScore -= number
     return this.answerScore = (this.answerScore < 0) ? 0 : this.answerScore
